dbb93b3dc7dc61966c68ef352ee7b38b027f4426
Cherrielyn Cuizon <cherrievirrey@yahoo.com>
2023-04-28 13:51:46 -0400

    First commit

---
+++ abadatacollector/src/main/java/com/cheery/abadatacollector/AbadatacollectorApplication.java
@@ -0,0 +1,13 @@
+package com.cheery.abadatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbadatacollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbadatacollectorApplication.class, args);
+	}
+
+}
---
+++ abadatacollector/src/test/java/com/cheery/abadatacollector/AbadatacollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheery.abadatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbadatacollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
